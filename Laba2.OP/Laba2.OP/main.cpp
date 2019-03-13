5c3ca1c4fc34238f8dd040362e408f4db93c9ac1
Passha182 <42549254+Passha182@users.noreply.github.com>
2019-03-13 02:18:59 +0200

    Update main.cpp

--- Laba2.OP/Laba2.OP/main.cpp
+++ Laba2.OP/Laba2.OP/main.cpp
@@ -8,5 +8,14 @@ int main() {
     string name = "premier_league.csv";
     string s;
     string comands[20];
-    return 0;
+    int score[20][20];
+	int points[20];
+	Comands(s, comands, name);
+	Score(s, score, name);
+	Points(points, score);
+	ofstream f("results.csv");
+	for (int i = 0; i < 20; i++) {
+		f << comands[i] << "-" << points[i] << endl;
+	}
+	return 0;
 }
