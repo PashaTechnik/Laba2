53eb3acc27bed3a73051430adbc6fea53ecfdfdc
Passha182 <42549254+Passha182@users.noreply.github.com>
2019-03-13 02:17:18 +0200

    Update Header.h

--- Laba2.OP/Laba2.OP/Header.h
+++ Laba2.OP/Laba2.OP/Header.h
@@ -6,4 +6,6 @@
 #include <string>
 using namespace std;
 void Commands(string s, string commands[], string name);
+void Score(string s, int score[20][20], string name);
+void Points(int points[], int score[20][20]);
 #endif
